693ed66285d14b9c84a4f0d20e275da0b354a3d3
Prasad <prasadwakle026@gmail.com>
2022-07-13 20:56:06 +0530

    Modified ML model, indexSVM.py, main.py

--- main.py
+++ main.py
@@ -2,8 +2,14 @@
 import numpy as np
 import pickle
 
+
+#SVM
 # loading the saved model
-loaded_model = pickle.load(open('C:/Users/prasa/OneDrive/Desktop/Diabetes/trained_model.sav', 'rb'))
+loaded_model_svm = pickle.load(open('C:/Users/prasa/OneDrive/Desktop/Diabetes/trained_model_svm.sav', 'rb'))
+
+#RFC
+# loading the saved model
+loaded_model_rfc = pickle.load(open('C:/Users/prasa/OneDrive/Desktop/Diabetes/trained_model_rfc.sav', 'rb'))
 
 
 input_data = (5,166,72,19,175,25.8,0.587,51)
@@ -14,10 +20,29 @@ input_data_as_numpy_array = np.asarray(input_data)
 # reshape the array as we are predicting for one instance
 input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
 
-prediction = loaded_model.predict(input_data_reshaped)
-print(prediction)
+#SVM Prediction
+prediction_svm = loaded_model_svm.predict(input_data_reshaped)
+print(prediction_svm)
 
-if (prediction[0] == 0):
-  print('The person is not diabetic')
+if (prediction_svm[0] == 0):
+  print('The person is not diabetic(SVM)')
 else:
-  print('The person is diabetic')
+  print('The person is diabetic(SVM)')
+
+
+#RFC Prediction
+prediction_rfc = loaded_model_rfc.predict(input_data_reshaped)
+print(prediction_rfc)
+
+if (prediction_rfc[0] == 0):
+  print('The person is not diabetic(RFC)')
+else:
+  print('The person is diabetic(RFC)')
+
+
+
+
+
+
+
+
